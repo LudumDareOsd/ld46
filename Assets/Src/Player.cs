7caae156c829918f0beeb67613758173b1d8a390
Emil Sunesson <sunesson.emil@gmail.com>
2020-04-18 12:58:44 +0200

    Weapons

--- Assets/Src/Player.cs
+++ Assets/Src/Player.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 	private Rigidbody2D body;
+	private Weapon weapon;
 
 	public void Start()
 	{
 		body = GetComponent<Rigidbody2D>();
+		weapon = GetComponentInChildren<Weapon>();
 	}
 
 	void Update()
@@ -16,17 +18,18 @@ public class Player : MonoBehaviour
 		var moveHorizontal = Input.GetAxis("Horizontal");
 		var moveVertical = Input.GetAxis("Vertical");
 
-		Debug.Log(moveVertical);
 		body.AddForce(5 * new Vector2(moveHorizontal, moveVertical));
 		body.velocity = body.velocity * 0.9f;
 
-
 		var positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
 		var mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
-
-		var angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+		var angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen) + 90f;
 
 		transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+
+		if (Input.GetButton("Fire1")) {
+			weapon.Fire(Time.deltaTime);
+		}
 	}
 
 	float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
---
+++ Assets/Src/PlayerBullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+	void Start()
+	{
+		Destroy(gameObject, 2);
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.layer == 9)
+		{
+			// collision.gameObject.GetComponent<Car>().TakeDamage(10 + Random.Range(-2, 2));
+			Destroy(gameObject);
+		}
+	}
+}

f210bbdf9e32b44e91bc0481b6d03227edecd20c
Emil Sunesson <sunesson.emil@gmail.com>
2020-04-18 19:10:19 +0200

    hpbar

---
+++ Assets/HpBar.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HpBar : MonoBehaviour
+{
+	public float hp = 0;
+
+	public void SetHp(float hp) {
+		this.hp = hp;
+	}
+
+}

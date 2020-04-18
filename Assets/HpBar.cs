066851723f8a29ebbccddbbabe400a47d2cc38e5
Emil Sunesson <sunesson.emil@gmail.com>
2020-04-18 20:00:09 +0200

    Walls hp bar

--- Assets/HpBar.cs
+++ Assets/HpBar.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class HpBar : MonoBehaviour
 {
-	public float hp = 0;
+	public float maxHp = 10f;
+	public GameObject hpBar;
+
+	public void Start()
+	{
+		SetHp(maxHp);
+	}
+
+	public void SetMaxHp(float hp)
+	{
+		maxHp = hp;
+	}
 
 	public void SetHp(float hp) {
-		this.hp = hp;
+		hpBar.transform.localScale = new Vector3(hp / maxHp, 1, 1);
 	}
 
 }

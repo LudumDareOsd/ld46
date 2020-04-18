5fe16d2a5619fbed9dd519e651ef362e9cd643d3
Pontus <pontus@minabastapolare.se>
2020-04-18 11:18:31 +0200

    Added some basic stuff for enemies

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+	public GameObject enemyContainer;
+	public GameObject grunt;
+
+	void Start()
+    {
+		Debug.Log(enemyContainer);
+		Debug.Log(grunt);
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Src/Enemies/Grunt.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grunt : MonoBehaviour
+{
+	void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
